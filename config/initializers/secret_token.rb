86152d2f2a16f4f09406df3073a87cd9ddd5df08
musik <860577@gmail.com>
2013-07-07 22:51:21 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhaosheng::Application.config.secret_token = 'f6f063407dfcb9f47f9ac59b33240aff5b1e45628ce253ae6fdbeedeb18f30f88645dd3bcd9b31f9a9070e47bea87a36bed0679171c52042e97aba16752aa4ac'
